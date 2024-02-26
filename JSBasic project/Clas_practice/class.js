d86319972f14d7e8ab7633fe8297a1ef7f9af998
Ubivec228 <Ahmed_he91@itstep.edu.az>
2024-02-26 22:29:21 +0400

    Practise with __proto__

--- JSBasic project/Clas_practice/class.js
+++ JSBasic project/Clas_practice/class.js
@@ -1,80 +1,24 @@
-// const animal = {
-//     name: "Animal",
-//     age: 5,
-//     hasTail: true
-// }
+let geom = {
+    name: "Pramougolnik",
+    sp: { x: 0, y: 0 },
+    ep: { x: 100, y: 200 },
+    get geomInfo() { return this.name },
+    set geomInfo(name) { this.name = name }
+}
 
-// class Animal { 
-//     static type = "ANIMAL"
-//     constructor(options) { 
-//         this.name= options.name,
-//         this.age = options.age,
-//         this.hasTail = options.hasTail
-//     }
-//     voice(){ 
-//         console.log("I'm Animal")
-//     }
-// }
-
-// // const animal = new Animal({ 
-// //     name: " Animal",
-// //     age: 5,
-// //     hasTail: true
-// // });
-
-
-// class Cat extends Animal {
-//     static type = "CAT"
-
-//     constructor(options){ 
-//         super(options)
-//         this.color = options.color
-//     }
-//     voice() { 
-//         super.voice()
-//         console.log("I'm Cat");
-//     }
-//     get ageInfo() { 
-//         return this.age * 7
-//     }
-//     set ageInfo(newAge) { 
-//         this.age = newAge
-//     }
-// }
-
-// const cat = new Cat({
-//     name: "Cat",
-//     age:7,
-//     hasTail:true
-// })
-
-
-class Component { 
-    constructor(selector) { 
-        this.$el = document.querySelector(selector)
-    }
-    hide() { 
-        this.$el.style.display  ="none"
-    }
-    show() { 
-        this.$el.style.display  ="block"
-    }
-}   
-
-
-class Box extends Component { 
-    constructor(options)  {
-        super(options.selector)
-
-        this.$el.style.width = this.$el.style.height = options.size + 'px'
-        this.$el.style.background = options.color
+let rect = {
+    draw() {
+        console.log("Pramougolnik : " +
+            this.sp.x + "," + this.sp.y + "," + this.ep.y + "," + this.ep.y)
     }
 }
 
-const box1 = new Box({
-    selector:"#box1",
-    size: 100,
-    color:"red",
-})
+rect.__proto__ = geom;
+console.log(rect.geomInfo)
+rect.geomInfo = "Novruz"
+console.log(rect.geomInfo);
 
-
+for (let prop in rect) {
+    if (rect.hasOwnProperty(prop))
+        console.log(prop + ":" + rect[prop])
+}
