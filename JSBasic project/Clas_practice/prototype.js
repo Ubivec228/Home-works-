2b27f3f0457aca6faf7ff620a7dd7fca5fd3d558
Ubivec228 <Ahmed_he91@itstep.edu.az>
2024-02-27 19:19:32 +0400

    muallim che to napisal

--- JSBasic project/Clas_practice/prototype.js
+++ JSBasic project/Clas_practice/prototype.js
@@ -16,23 +16,65 @@ function Rect() {
     this.draw = function () {
         console.log("Risovaniye figuri : " + this.name);
     };
-    // this.__proto__ = prop;
+    this.x = prop;
 }
 
-Rect.prototype.__proto__ = prop;
-Rect.prototype.getName = function () {return this.name}
-Rect.prototype.setName = function (name) {this.name = name}
+// Rect.prototype.__proto__ = prop;
+// Rect.prototype.getName = function () {return this.name}
+// Rect.prototype.setName = function (name) {this.name = name}
 
 // Rect.prototype = prop;
 let r = new Rect();
 
+// console.log(r.__proto__ === Rect.prototype)
 // console.log(Rect.prototype)
-console.log(r);
-// r.prototype = "hello";
+// console.log(r);
+r.prototype = "hello";
 
 // r.draw();
 
-// for (let p in r)
-//     console.log(p + ': ' + r[p]);
+for (let p in r) {
+    console.log(p + ': ' + r[p]);
+
+    if (p === 'x') {
+        for (const key in r[p]) {
+            console.log('AAAAAAA', r[p][key]);
+        }
+    }
+}
 
 
+
+// const x = {
+//     a: 5,
+//     prototype: {
+//         e: 8,
+//         o: 5,
+//         p: 8,
+//         d: 90,
+//         q: 1,
+//         w: 2,
+//         e: 3,
+//         r: 4,
+//         t: 5,
+//         j: {
+//             k: 8,
+//             m: 98,
+//             a: {
+//                 k: 67,
+//                 u: 67
+//             }
+//         }
+//     },
+//     __proto__: {
+//         b: 6,
+//         __proto__: {
+//             c: 7
+//         }
+//     }
+// }
+
+
+// for (const key in x) {
+//     console.log(key, x[key]);
+// }
